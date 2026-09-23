e62a2a8531cf04286f335bb7071e257c481c5201
arturoalbero <aag400@gmail.com>
2026-09-23 10:32:00 +0200

    dart improved

---
+++ TEST/DARTeos/mi_app/bin/mi_app.dart
@@ -0,0 +1,5 @@
+import 'package:mi_app/mi_app.dart' as mi_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mi_app.calculate()}!');
+}
---
+++ TEST/DARTeos/mi_app/test/mi_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:mi_app/mi_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
